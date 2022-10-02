29bee24f4f6688fad811648ad8b05de45b70d582
NoamAnisfeld <NoamAnisfeld@gmail.com>
2022-10-02 22:38:35 +0300

    adding buttons

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -4,6 +4,10 @@ export default function Popup({
     imageURL: string
 }) {
     return <div className="popup">
+        <div className="controls">
+            <button type="button">-</button>
+            <button type="button">+</button>
+        </div>
         <div className="container">
             <img className="image" src={imageURL} alt="" />
         </div>
